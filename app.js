8892f122e65aa4255f47ed8755ac0480c768752b
hamza-naeem=098 <hamzanaeem672@gmail.com>
2024-02-28 03:17:03 +0500

    one file html and one file js

---
+++ app.js
@@ -0,0 +1,205 @@
+
+                             // Home
+// // chapter 6
+// // Q1
+// x++;
+// // this is llegal Expression
+// // Q2
+// var num=100;
+// console.log(--num);
+// // Q3
+// var x=50;
+// var y=x++;
+// console.log(y);
+// // ans is 50
+// // Q4
+// var y=50;
+// var z=y--;
+// console.log(z);
+// // ans is 50
+// // Q5
+// var newNum = num = num - 1;
+// console.log(newNum);
+// // Q6
+//  var num = 5;
+//  var newNum = ++num; 
+//  console.log(newNum);
+// //  Q7
+// var num =20;
+// num++;
+// alert("the new num is:" + num);
+
+
+
+// // // chapter 7
+// // // Q1
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum);
+// // // Q2
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum);
+// // // Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+// // // Q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+// // // Q5
+// var calculatedNum = (2 + 2) * (4 + 10);
+// console.log(calculatedNum);
+// // // Q6
+// var calculatedNum = 2 + 2 * 4 + 10;
+// console.log(calculatedNum);
+// // // Q7
+// var calculatedNum = 4 / 2 * 4;
+// console.log(calculatedNum);
+// // // producing 5 is not possible
+
+// // // chapter 8
+// // // Q1
+// var num = "2" + "2";
+// console.log(num);
+// // // Q2
+// var message = ("Hello " + "Dolly");
+// console.log(message);
+// // // Q3
+// alert("33" + 3);
+// // // Q4
+// alert("Pakistan  " + "Zindabad");
+// // // Q5
+// var Name = "haider";
+// var num = "55"
+// alert(Name + num);
+// // // Q6
+// var num = "10";
+// var num2 = "20";
+// var result = (num + num2);
+// console.log(result);
+// // // chapter 9
+// // // Q1
+// var Name = prompt("Enter your name");
+// document.write(Name);
+// // // Q2
+// var country = prompt("Country?", "China");
+// document.write(country);
+// // // Q3
+// var yourName = prompt("Enter Your Name");
+// // // Q4
+// var userName = prompt(" enter your name:", "Haider khan");
+// document.write(userName);
+// // // Q5
+// var message = "Hello"
+// var defaultResponse = "iam hamza";
+// var userResponse = prompt(message , defaultResponse);
+// // // Q6
+// var message = "Hello"
+// var defaultResponse = "iam hamza";
+// var userResponse = prompt(message , defaultResponse);
+// alert(userResponse);
+
+
+
+
+// Chapter 4 
+
+// //Q1
+// var x, y, z = ("10, 20, 30")
+// //Q2
+// // Legal Variable Names:
+// age
+// myVar
+// _count
+// firstName
+// totalAmount
+
+// //Illegal Variable Names:
+
+// 123var (cannot start with a number)
+// my-var (cannot contain hyphens)
+// $money (cannot start with special characters other than underscore)
+// my variable (cannot contain spaces)
+// class (reserved keyword in many programming languages)
+
+// //Q3
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable names can only contain <strong>letters</strong>, <strong>digits</strong>, <strong>underscores</strong>, and <strong>dollar signs</strong>. For example <code>$my_1stVariable</code>.</p>");
+// document.write("<p>Variables must begin with a <strong>letter</strong>, <strong>underscore</strong>, or <strong>dollar sign</strong>. For example <code>$name</code>, <code>_name</code>, or <code>name</code>.</p>");
+// document.write("<p>Variable names are case <strong>sensitive</strong>.</p>");
+// document.write("<p>Variable names should not be JS <strong>keywords</strong>.</p>");
+
+
+//Chapter 5  MATH EXPRESSIONS
+
+// //Q1
+// a = 3;
+// b = 5;
+// c= a + b;
+// document.write(c)
+// console.log(c)
+
+// Q2
+// //Subtraction function
+// a = 3;
+// b = 5;
+// c= a - b;
+// console.log(c)
+
+// //Multiplication function
+// a = 3;
+// b = 5;
+// c= a * b;
+// console.log(c)
+
+// //Division function
+// a = 3;
+// b = 5;
+// c= a / b;
+// console.log(c)
+
+// //Modulus function
+// a = 3;
+// b = 5;
+// c= a % b;
+// console.log(c)
+
+
+// //Q3
+// var myVariable;
+// document.write("Value after variable declaration is: " + myVariable + "<br>");
+// myVariable = 5;
+// document.write("Initial value: " + myVariable + "<br>");
+// myVariable++;
+// myVariable += 7;
+// document.write("Value after addition is: " + myVariable + "<br>");
+// document.write("Value after decrement is: " + myVariable + "<br>");
+// var remainder = myVariable % 3;
+// document.write("The remainder is: " + remainder + "<br>");
+
+
+// //Q4
+
+// var ticket_price = 600 
+
+// var num_ticket = 5
+
+// var total_cost = ticket_price *  num_ticket 
+
+// console.log(total_cost)
+
+// document.write("The cost of buying 5 tickets to a movie is 3000 PKR.")
+
+// Q5
+
+// var num = prompt("Enter a number to generate a table")
+
+// document.write(num + " x  1  =  " + num + "<br>")
+// document.write(num + " x  2  =  " + num + "<br>")
+// document.write(num + " x  3  =  " + num + "<br>")
+// document.write(num + " x  4  =  " + num + "<br>")
+// document.write(num + " x  5  =  " + num + "<br>")
+// document.write(num + " x  6  =  " + num + "<br>")
+// document.write(num + " x  7  =  " + num + "<br>")
+// document.write(num + " x  8  =  " + num + "<br>")
+// document.write(num + " x  9  =  " + num + "<br>")
+// document.write(num + " x  10 =  " + num + "<br>")
+
